76b6cbe8ec16731546589818b97745be471332e4
aramg <aramg@users.noreply.github.com>
2022-11-28 21:21:31 -0800

    Version 2.0.1

--- src/plugin.cc
+++ src/plugin.cc
@@ -44,7 +44,7 @@ AddDevice* addDevUI = NULL;
 #include "buffer_util.h"
 #include "device_discovery.h"
 
-#define VERSION_TEXT "200"
+#define VERSION_TEXT "201"
 #define FPS 25
 #define MILLI_SEC 1000
 #define NANO_SEC  1000000000
