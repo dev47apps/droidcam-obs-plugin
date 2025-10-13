73ec2a01e234e6b2287866c25b4242dca6d9d2f6
Ruslan Galiullin <160370317+kjetteren@users.noreply.github.com>
2025-10-13 07:52:42 +0200

    Added support for libavcodec 62 (#29)

--- src/ffmpeg_decode.cc
+++ src/ffmpeg_decode.cc
@@ -120,7 +120,11 @@ int FFMpegDecoder::init(uint8_t* header, enum AVCodecID id, bool use_hw)
 		}
 
 		decoder->sample_rate = aac_frequencies[sr_idx];
-		decoder->profile = FF_PROFILE_AAC_LOW;
+		#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(62, 0, 0)
+    	decoder->profile = FF_PROFILE_AAC_LOW;
+		#else
+    	decoder->profile = AV_PROFILE_AAC_LOW;
+		#endif
 
 		const int channels = (header[1] >> 3) & 0xF;
 		#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(59, 24, 100)
