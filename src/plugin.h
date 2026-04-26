b476ff5dd58912180888a9999272554c2186e0b4
aramg <aramg@users.noreply.github.com>
2026-04-26 11:02:36 -0700

    Version 2.5.0

--- src/plugin.h
+++ src/plugin.h
@@ -2,7 +2,7 @@
 #pragma once
 #include <obs-module.h>
 
-#define PLUGIN_VERSION_STR "243"
+#define PLUGIN_VERSION_STR "250"
 
 #define xlog(log_level, format, ...) \
         blog(log_level, "[DroidCamOBS] " format, ##__VA_ARGS__)
